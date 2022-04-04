87e84f3a0a1b4f84fb3dc9b6d66f79bc2a87f8cd
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-04-04 15:45:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const contactRoute = require("./route/contactRoute");
+const ContactRoute = require("./route/ContactRoute");
 
 const app = express();
 
