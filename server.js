a9cc00e66fe5c7782b9e659a96876596764eb86f
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-04-04 16:39:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
+// const port = process.env.PORT || 5000;
+// app.listen(port, console.log(`server listing to port 5000 only`));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server port ${PORT}`);
