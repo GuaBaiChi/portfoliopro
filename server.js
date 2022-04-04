b646b10bf5a328ca757a7790e3f67f2bddf54a49
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-04-04 16:49:42 -0400

    server.js completely changed

--- server.js
+++ server.js
@@ -12,12 +12,11 @@ app.use(cors());
 app.use("/", ContactRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 // const port = process.env.PORT || 5000;
 // app.listen(port, console.log(`server listing to port 5000 only`));
 
