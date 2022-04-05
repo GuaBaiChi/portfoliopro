7cae399c27ad2b2463c3c36f87dcf8ed84dff147
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-04-05 13:57:08 -0400

    heroko stuff

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use("/", ContactRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "build/public")));
 }
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
